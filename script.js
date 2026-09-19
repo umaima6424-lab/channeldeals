fb6a898ed561c4cc696a6741dfdfd248c08a1c52
Umama javed <umaima6424@gmail.com>
2026-09-19 14:11:58 +0500

    Update script.js

--- script.js
+++ script.js
@@ -127,7 +127,7 @@ buyButtons.forEach(button => {
                     `
                 }
 
-                <a href="https://t.me/TubeYt"
+                <a href="https://t.me/tubeyt2"
                    target="_blank"
                    class="purchase-option purchase-telegram">
                     <i class="fa-brands fa-telegram"></i>
